191edd0dd5402f85b343a6bf9fff1918856474a0
MohaamedSaaied <mohamedelokr20@gmail.com.com>
2024-07-07 20:08:07 +0300

    Add project files.

---
+++ point.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment__1__OOP
+{
+    internal class point
+    {
+        public int X; public int Y;
+    }
+}
